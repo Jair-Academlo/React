3b6a7a636165ddc85cf9012f862875c3cc57e72d
sasuske <jelver00@hotmail.com>
2022-02-27 19:47:51 -0500

    Firts-App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+/*import User from "./componentes/User";*/
+import Quotes from "./componentes/QuotesBox";
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Quotes />
+    </div>
+  );
+ 
+
+}
+
+export default App;
